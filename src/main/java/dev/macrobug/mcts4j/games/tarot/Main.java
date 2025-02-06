741e59273458345237972bd0117391c6cdd2b41f
Davide Rizzi <150713+sadjehwty@users.noreply.github.com>
2025-02-06 09:03:47 +0100

    Duplicata struttura per nuovo gioco

---
+++ src/main/java/dev/macrobug/mcts4j/games/tarot/Main.java
@@ -0,0 +1,28 @@
+package dev.macrobug.mcts4j.games.tarot;
+
+import io.github.nejc92.mcts.Mcts;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        State state = State.initialize(Player.Type.NOUGHT);
+        Mcts<State, String, Player> mcts = Mcts.initializeIterations(15000);
+        for(int turn=0;!state.isTerminal();turn++) {
+            String action;
+            if(turn%2==1) action = sc.nextLine();
+            else action = mcts.uctSearchWithExploration(state,1.5);
+            state.performActionForCurrentAgent(action);
+            char[][] board=state.getBoard();
+            for (char[] chars : board) {
+                for (char aChar : chars) {
+                    System.out.print(aChar + " ");
+                }
+                System.out.println();
+            }
+            if(state.isDraw()){System.out.println("===================");}
+            else if(!state.isTerminal()){System.out.println("-------------------");}
+        }
+    }
+}

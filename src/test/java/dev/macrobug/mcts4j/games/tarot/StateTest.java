6717e3d6c64fbedcfe18159ed7290e7b0ef34150
Davide Rizzi <150713+sadjehwty@users.noreply.github.com>
2025-02-10 11:47:25 +0100

    iniziati junit

---
+++ src/test/java/dev/macrobug/mcts4j/games/tarot/StateTest.java
@@ -0,0 +1,15 @@
+package dev.macrobug.mcts4j.games.tarot;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class StateTest {
+    @Test
+    public void getPointsTest() {
+        // NESSUNA PRESA
+        assertEquals(0, State.getPoints(new ArrayList<>()));
+    }
+}

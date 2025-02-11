3d171d57f723bf491f3b3a5639b96171c28eb1b3
Davide Rizzi <150713+sadjehwty@users.noreply.github.com>
2025-02-11 10:57:50 +0100

    test sul cappotto

--- src/test/java/dev/macrobug/mcts4j/games/tarot/StateTest.java
+++ src/test/java/dev/macrobug/mcts4j/games/tarot/StateTest.java
@@ -3,6 +3,8 @@ package dev.macrobug.mcts4j.games.tarot;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -11,5 +13,12 @@ public class StateTest {
     public void getPointsTest() {
         // NESSUNA PRESA
         assertEquals(0, State.getPoints(new ArrayList<>(),new Game(0)));
+        // CAPPOTTO
+        assertEquals(0, State.getPoints(new ArrayList<>(List.of(Card.MATTO)),new Game(0)));
+        ArrayList<Card> cards = new ArrayList<>(State.allCards);
+        Game game=new Game(0,new Card[]{Card.MORETTO,Card.MORETTO,Card.MORETTO,Card.MORETTO});
+        assertEquals(978, State.getPoints(cards,game));
+        cards.remove(Card.MATTO);
+        assertEquals(978, State.getPoints(cards,game));
     }
 }

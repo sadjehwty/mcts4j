90b418d5cc69ffb7f063544e58f6ecd6cf4f7eaa
Davide Rizzi <150713+sadjehwty@users.noreply.github.com>
2025-02-13 16:32:29 +0100

    test starto

--- src/test/java/dev/macrobug/mcts4j/games/tarot/StateTest.java
+++ src/test/java/dev/macrobug/mcts4j/games/tarot/StateTest.java
@@ -26,9 +26,49 @@ public class StateTest {
     @Test
     public void scartataIniziale() {
         Player player = new Player(new ArrayList<>(List.of(
-                Card.MATTO)),0);
-        assertArrayEquals(new ArrayList<Card>().toArray(), State.scartataIniziale(player).toArray());
+                new Card(14,Suit.BASTONI),
+                new Card(1,Suit.BASTONI),
+                new Card(10,Suit.COPPE),
+                new Card(9,Suit.COPPE),
+                new Card(8,Suit.SPADE),
+                new Card(13,Suit.DENARI),
+                new Card(19,Suit.TRIONFI),
+                new Card(18,Suit.TRIONFI),
+                new Card(17,Suit.TRIONFI),
+                new Card(16,Suit.TRIONFI),
+                new Card(15,Suit.TRIONFI),
+                new Card(14,Suit.TRIONFI),
+                new Card(13,Suit.TRIONFI),
+                new Card(12,Suit.TRIONFI),
+                new Card(2,Suit.TRIONFI),
+                new Card(1,Suit.TRIONFI),
+                new Card(0,Suit.TRIONFI))),0);
+        assertArrayEquals(new ArrayList<>(List.of(
+                new Card(8,Suit.SPADE),
+                new Card(13,Suit.DENARI)
+        )).toArray(), State.scartataIniziale(player).toArray());
+
+        player = new Player(new ArrayList<>(List.of(
+                new Card(14,Suit.BASTONI),
+                new Card(1,Suit.BASTONI),
+                new Card(10,Suit.COPPE),
+                new Card(9,Suit.COPPE),
+                new Card(13,Suit.DENARI),
+                new Card(12,Suit.DENARI),
+                new Card(11,Suit.DENARI),
+                new Card(10,Suit.DENARI),
+                new Card(17,Suit.TRIONFI),
+                new Card(16,Suit.TRIONFI),
+                new Card(15,Suit.TRIONFI),
+                new Card(14,Suit.TRIONFI),
+                new Card(13,Suit.TRIONFI),
+                new Card(12,Suit.TRIONFI),
+                new Card(2,Suit.TRIONFI),
+                new Card(1,Suit.TRIONFI),
+                new Card(0,Suit.TRIONFI))),0);
+        assertArrayEquals(new ArrayList<>(List.of(
+                new Card(10,Suit.COPPE),
+                new Card(9,Suit.COPPE)
+        )).toArray(), State.scartataIniziale(player).toArray());
     }
-
-
 }

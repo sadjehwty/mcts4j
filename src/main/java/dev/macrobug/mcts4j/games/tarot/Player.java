ac279d57e19ae46eb64603154387affccf845af8
Davide Rizzi <150713+sadjehwty@users.noreply.github.com>
2025-02-20 15:54:29 +0100

    come scartare dipende dal giocatore

--- src/main/java/dev/macrobug/mcts4j/games/tarot/Player.java
+++ src/main/java/dev/macrobug/mcts4j/games/tarot/Player.java
@@ -37,6 +37,59 @@ public class Player implements MctsDomainAgent<State> {
         return getRandomActionFromActions(availableActions);
     }
 
+    public List<Card> scartataIniziale() {
+        ArrayList<Card> carte=getDeck();
+        record Statistica(boolean capo, int count, Card ultima, Card penultima) implements Comparable<Statistica>{
+            @Override
+            public int compareTo(Statistica s) {
+                if(!this.capo && this.count==1 && !(!s.capo && s.count==1)) return -1;
+                if(!this.capo && this.count==2 && (s.capo || s.count>2)) return -1;
+                if(!this.capo && s.capo) return -1;
+                if(!s.capo && s.count==1 && !(!this.capo && this.count==1)) return 1;
+                if(!s.capo && s.count==2 && (this.capo || this.count>2)) return 1;
+                if(!s.capo && this.capo) return 1;
+                return 1-Integer.compare(this.count,s.count);
+            }
+        }
+        List<Statistica> carteStatistica = new ArrayList<>();
+        List<Card> scartate = new ArrayList<>();
+        for(Suit suit: Suit.values()){
+            int capo=suit.equals(Suit.TRIONFI)?20:14;
+            List<Card> cards = carte.stream().filter((c)->c.suit().equals(suit) && !c.equals(Card.MATTO) && !c.equals(Card.BEGATTO)).sorted().toList();
+            if(!cards.isEmpty())
+                carteStatistica.add(new Statistica(cards.contains(new Card(capo,suit)),cards.size(), cards.get(0),cards.size()>1?cards.get(1):null));
+        }
+        ArrayList<Statistica> singole=new ArrayList<>(carteStatistica.stream().sorted().toList());
+        while(scartate.size()<2){
+            Statistica s = singole.removeFirst();
+            if(s.ultima!=null &&
+                    !s.ultima.equals(Card.MATTO) &&
+                    !s.ultima.equals(Card.BEGATTO) &&
+                    !s.ultima.equals(Card.MONDO) &&
+                    !s.ultima.equals(Card.ANGELO) && (
+                    s.ultima.suit().equals(Suit.TRIONFI) ||
+                            s.ultima.value()<14
+            )){
+                scartate.add(s.ultima);
+                getDeck().remove(s.ultima);
+            }
+            if(scartate.size()==2) continue;
+            if(s.penultima!=null &&
+                    !s.penultima.equals(Card.MATTO) &&
+                    !s.penultima.equals(Card.BEGATTO) &&
+                    !s.penultima.equals(Card.MONDO) &&
+                    !s.penultima.equals(Card.ANGELO) && (
+                    s.penultima.suit().equals(Suit.TRIONFI) ||
+                            s.penultima.value()<14
+            )){
+                scartate.add(s.penultima);
+                getDeck().remove(s.penultima);
+            }
+        }
+        return scartate;
+    }
+
+
     private boolean actionWinsGame(State state, Card action) {
         state.performActionForCurrentAgent(action);
         boolean actionEndsGame = state.isTerminal();
--- src/main/java/dev/macrobug/mcts4j/games/tarot/State.java
+++ src/main/java/dev/macrobug/mcts4j/games/tarot/State.java
@@ -174,9 +174,9 @@ public class State implements MctsDomainState<Card, Player> {
         // azione dello scartatore
         // per ora lo facciamo statico
         if (firstPlayer % 2 == 0)
-            state.preseNS.addAll(scartataIniziale(players[firstPlayer]));
+            state.preseNS.addAll(players[firstPlayer].scartataIniziale());
         else
-            state.preseEW.addAll(scartataIniziale(players[firstPlayer]));
+            state.preseEW.addAll(players[firstPlayer].scartataIniziale());
         return state;
     }
 
@@ -215,58 +215,6 @@ public class State implements MctsDomainState<Card, Player> {
             players[(i+delta)%4] = new Player(x,i);
         }
         return players;
-    }
-
-    public static List<Card> scartataIniziale(Player player) {
-        ArrayList<Card> carte=player.getDeck();
-        record Statistica(boolean capo, int count, Card ultima, Card penultima) implements Comparable<Statistica>{
-            @Override
-            public int compareTo(Statistica s) {
-                if(!this.capo && this.count==1 && !(!s.capo && s.count==1)) return -1;
-                if(!this.capo && this.count==2 && (s.capo || s.count>2)) return -1;
-                if(!this.capo && s.capo) return -1;
-                if(!s.capo && s.count==1 && !(!this.capo && this.count==1)) return 1;
-                if(!s.capo && s.count==2 && (this.capo || this.count>2)) return 1;
-                if(!s.capo && this.capo) return 1;
-                return 1-Integer.compare(this.count,s.count);
-            }
-        }
-        List<Statistica> carteStatistica = new ArrayList<>();
-        List<Card> scartate = new ArrayList<>();
-        for(Suit suit: Suit.values()){
-            int capo=suit.equals(Suit.TRIONFI)?20:14;
-            List<Card> cards = carte.stream().filter((c)->c.suit().equals(suit) && !c.equals(Card.MATTO) && !c.equals(Card.BEGATTO)).sorted().toList();
-            if(!cards.isEmpty())
-                carteStatistica.add(new Statistica(cards.contains(new Card(capo,suit)),cards.size(), cards.get(0),cards.size()>1?cards.get(1):null));
-        }
-        ArrayList<Statistica> singole=new ArrayList<>(carteStatistica.stream().sorted().toList());
-        while(scartate.size()<2){
-            Statistica s = singole.removeFirst();
-            if(s.ultima!=null && 
-                    !s.ultima.equals(Card.MATTO) && 
-                    !s.ultima.equals(Card.BEGATTO) && 
-                    !s.ultima.equals(Card.MONDO) && 
-                    !s.ultima.equals(Card.ANGELO) && (
-                        s.ultima.suit().equals(Suit.TRIONFI) ||
-                        s.ultima.value()<14
-                    )){
-                scartate.add(s.ultima);
-                player.getDeck().remove(s.ultima);
-            }
-            if(scartate.size()==2) continue;
-            if(s.penultima!=null &&
-                    !s.penultima.equals(Card.MATTO) &&
-                    !s.penultima.equals(Card.BEGATTO) &&
-                    !s.penultima.equals(Card.MONDO) &&
-                    !s.penultima.equals(Card.ANGELO) && (
-                        s.penultima.suit().equals(Suit.TRIONFI) ||
-                        s.penultima.value()<14
-            )){
-                scartate.add(s.penultima);
-                player.getDeck().remove(s.penultima);
-            }
-        }
-        return scartate;
     }
 
     @Override

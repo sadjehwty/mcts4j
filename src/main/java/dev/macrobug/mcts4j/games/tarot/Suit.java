473fb4a2faeab5312d894a15423947927c3f1f67
sadjehwty <150713+sadjehwty@users.noreply.github.com>
2025-02-23 17:48:18 +0100

    pulizia codice

--- src/main/java/dev/macrobug/mcts4j/games/tarot/Suit.java
+++ src/main/java/dev/macrobug/mcts4j/games/tarot/Suit.java
@@ -7,16 +7,6 @@ public enum Suit{
     BASTONI,
     TRIONFI;
 
-    public static Suit parse(String substring) {
-        return switch (substring.toUpperCase().toCharArray()[0]){
-            case 'S'->SPADE;
-            case 'C'->COPPE;
-            case 'D'->DENARI;
-            case 'B'->BASTONI;
-            case 'T'->TRIONFI;
-            default -> throw new RuntimeException();
-        };
-    }
     public String toString(){
         return this.name().toUpperCase().substring(0,1);
     }
